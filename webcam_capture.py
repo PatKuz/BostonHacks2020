cf675a0c0cd012747a5df715e230bd3e66f45f69
justin-sayah <jsayah@bu.edu>
2020-11-14 14:50:59 -0500

    stream continues to take new photos

--- webcam_capture.py
+++ webcam_capture.py
@@ -1,17 +1,25 @@
 '''
-Opens a webcam window, and awaits user to input 's'.
+Opens a webcam window, and awaits user to input 'q'.
 
 Saves a screenshot in running dir and closes
 '''
-
 import cv2
+import time
+
 camera = cv2.VideoCapture(0)
+lastCapture = time.time()
+
+def checkImg(curTime):
+    if(time.time() - curTime > 1):
+        cv2.imwrite('test.jpg',image)
+        lastCapture = time.time()
+
 while True:
     return_value,image = camera.read()
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('image',gray)
-    if cv2.waitKey(1)& 0xFF == ord('s'):
-        cv2.imwrite('test.jpg',image)
-        break
+    cv2.imshow('image',image) 
+    checkImg(lastCapture)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
 camera.release()
 cv2.destroyAllWindows()
