0883718b76b74f16b63317cc48141237592180aa
Patrick Kuzdzal <pkuzdzal@bu.edu>
2020-11-14 21:13:19 -0500

    Evaluation framework done

--- evaluation.py
+++ evaluation.py
@@ -1,24 +1,31 @@
-import sys
+import sys, yaml
+from google.cloud import automl
 
-from google.cloud import automl_v1beta1
-from google.cloud.automl_v1beta1.proto import service_pb2
-
-
-def get_prediction(content, project_id, model_id):
-    prediction_client = automl_v1beta1.PredictionServiceClient()
-
-    name = 'projects/{}/locations/us-central1/models/{}'.format(project_id, model_id)
-    payload = {'image': {'image_bytes': content }}
-    params = {}
-    request = prediction_client.predict(name, payload, params)
-    return request
-
-if __name__ == '__main__':
-    file_path = sys.argv[1]
-    project_id = sys.argv[2]
-    model_id = sys.argv[3]
+def get_prediction(file_path):
+    prediction_client = automl.PredictionServiceClient()
+    creds = yaml.safe_load(open("creds.yaml", "r"))
+    project_id = creds['PROJ_ID']
+    model_id = creds['MODEL_ID']
 
     with open(file_path, 'rb') as ff:
         content = ff.read()
 
-        print get_prediction(content, project_id, model_id)
+    name = 'projects/{}/locations/us-central1/models/{}'.format(project_id, model_id)
+    payload = {'image': {'image_bytes': content }}
+    params = {"score_threshold": "0.501"}
+    request = automl.PredictRequest(
+        name=name,
+        payload=payload,
+        params=params
+    )
+    response = prediction_client.predict(request=request)
+    coords = []
+    pred = ''
+    for item in response.payload:
+        for vert in item.image_object_detection.bounding_box.normalized_vertices:
+            coords.append(vert)
+        pred = item.display_name
+    if pred!='':
+        return pred,coords[0].x,coords[0].y,coords[1].x,coords[1].y
+    else:
+        return None
