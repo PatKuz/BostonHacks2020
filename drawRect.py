87ddc2f2dac4be2ababfe2f5334582a956d95e9a
Patrick Kuzdzal <pkuzdzal@bu.edu>
2020-11-15 10:47:08 -0500

    Finishing Touches

--- drawRect.py
+++ drawRect.py
@@ -8,8 +8,8 @@ top left and bottom right veritces
 def drawRect(file_path,label,x1,y1,x2,y2):
     im = Image.open(file_path)
     draw = ImageDraw.Draw(im)
-    vertices = ((x1*640,y1*480),(x1*640,y2*480),(x2*640,y2*480),(x2*640,y1*480))
+    vertices = ((x1*1280,y1*720),(x1*1280,y2*720),(x2*1280,y2*720),(x2*1280,y1*720))
     draw.polygon(vertices,fill=None,outline=(0,255,255))
-    draw.text((x1*640,y1*480), label)
+    draw.text((x1*1280,y1*720), label)
     im.save('Archived/bounded_frame.jpg', quality=95)
     return 'Archived/bounded_frame.jpg'
