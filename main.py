567f8b2f4c777361a401ed80c0e47f1f50c025c4
justin-sayah <jsayah@bu.edu>
2020-11-14 21:15:21 -0500

    added comments to main

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from webcam_capture import getVideoFrame
 from send_sms import send_message
+from evaluation import get_prediction
+import time
 
 #starts webcam and begins implicit screenshot capture
+#to screenshot folder
 getVideoFrame()
 
-
+'''
+ - passes frame to trained ML model and returns guess, if mask is detected,
+   sends message to client and sleeps the guessing loop for 30 seconds.
+   Moves the photo that is detected immediately to a holding folder.
+'''
+label,x1,y1,x2,y2 = get_prediction(file_path)
